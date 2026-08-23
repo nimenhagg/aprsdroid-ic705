4561d89293d25984587cd3337134e37a4bfb76d5
nimenhagg <ldsjljq@gmail.com>
2026-08-23 20:04:48 +0800

    fix(v1.5.0): wire R.id.about dialog explicitly in HubActivity and LogActivity

--- src/HubActivity.kt
+++ src/HubActivity.kt
@@ -145,6 +145,14 @@ class HubActivity : MainRecyclerActivity("hub", R.id.hub) {
                 StorageCleaner(this, storage) { onStopLoading() }.execute()
                 true
             }
+            R.id.about -> {
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.app_name)
+                    .setMessage(getString(R.string.build_version))
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
--- src/LogActivity.kt
+++ src/LogActivity.kt
@@ -134,6 +134,14 @@ class LogActivity : MainRecyclerActivity("log", R.id.log) {
                 StorageCleaner(this, storage) { onStopLoading() }.execute()
                 true
             }
+            R.id.about -> {
+                AlertDialog.Builder(this)
+                    .setTitle(R.string.app_name)
+                    .setMessage(getString(R.string.build_version))
+                    .setPositiveButton(android.R.string.ok, null)
+                    .show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
