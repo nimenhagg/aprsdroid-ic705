f08d7db24e82b6690faeb33de5252a27e2127978
nimenhagg <119995761+nimenhagg@users.noreply.github.com>
2026-08-26 13:13:28 +0800

    feat: initialize persistent diagnostics at process start

--- src/APRSdroidApplication.kt
+++ src/APRSdroidApplication.kt
@@ -1,28 +1,13 @@
 package org.aprsdroid.app
 
 import android.app.Application
-import com.google.android.material.color.DynamicColors
-import okhttp3.OkHttpClient
-import org.maplibre.android.MapLibre
-import org.maplibre.android.module.http.HttpRequestUtil
+import org.aprsdroid.app.diagnostic.AppLog
+import org.aprsdroid.app.diagnostic.NetworkEventLogger
 
 class APRSdroidApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        MapLibre.getInstance(this)
-        val releaseVersion = BuildConfig.VERSION_NAME.substringBefore(' ')
-        val mapUserAgent = "APRSdroid-IC705/$releaseVersion MapLibre/${BuildConfig.MAPLIBRE_VERSION} " +
-            "(+https://github.com/nimenhagg/aprsdroid-ic705)"
-        val mapHttpClient = OkHttpClient.Builder()
-            .addNetworkInterceptor { chain ->
-                val request = chain.request().newBuilder()
-                    .header("User-Agent", mapUserAgent)
-                    .build()
-                chain.proceed(request)
-            }
-            .build()
-        HttpRequestUtil.setOkHttpClient(mapHttpClient)
-        DynamicColors.applyToActivitiesIfAvailable(this)
-        ServiceNotifier.instance.setupChannels(this)
+        AppLog.init(this)
+        NetworkEventLogger.start(this)
     }
 }
