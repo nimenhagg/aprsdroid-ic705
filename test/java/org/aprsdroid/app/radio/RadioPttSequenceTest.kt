5fd050f29931c2333dd764e413cd7239e53164d7
nimenhagg <ldsjljq@gmail.com>
2026-09-25 23:01:23 +0800

    Fix RadioPttSequence test fake PTT state visibility

--- test/java/org/aprsdroid/app/radio/RadioPttSequenceTest.kt
+++ test/java/org/aprsdroid/app/radio/RadioPttSequenceTest.kt
@@ -55,7 +55,7 @@ class RadioPttSequenceTest {
         override val radio = RadioDescriptor(1, "Test", "Fake")
         override val capabilities = RadioCapabilities(canGetPtt = true, canSetPtt = true)
         override var isOpen: Boolean = true
-        private var pttState: Boolean = false
+        var pttState: Boolean = false
         val commands = mutableListOf<Boolean>()
 
         override fun open() = Unit
